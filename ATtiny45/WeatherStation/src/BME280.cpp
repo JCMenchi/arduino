1c7d3fb860aed8a3cebeab2f4c255895ff22142a
JC Menchi <jean.menchi@gmail.com>
2024-01-23 20:05:19 +0100

    Test impl of BME280

--- ATtiny45/WeatherStation/src/BME280.cpp
+++ ATtiny45/WeatherStation/src/BME280.cpp
@@ -36,7 +36,7 @@ equation courtesy of Brian McNoldy at http://andrew.rsmas.miami.edu.
 // BME280 configuration
 #define TEMP_SAMPLING 3 // use 4x sampling
 #define HUM_SAMPLING 3  // use 4x sampling
-#define PRES_SAMPLING 3 // use 4x sampling
+#define PRES_SAMPLING 5 // use 16x oversampling
 
 #define Mode_Sleep 0
 #define Mode_Forced 1
@@ -44,23 +44,77 @@ equation courtesy of Brian McNoldy at http://andrew.rsmas.miami.edu.
 
 #define BME280_MODE Mode_Normal
 #define BME280_STANDBY_TIME 5 // 1000 ms
-#define BME280_FILTER 3       // Filter 8
+#define BME280_FILTER 5       // Filter 16
 #define BME280_FILTER_OFF 1
 
-/****************************************************************/
-void BME280::InitializeFilter() {
-  // Force an unfiltered measurement to populate the filter buffer.
-  // This fixes a bug that causes the first read to always be 28.82 °C 81732.34
-  // hPa.
-  WriteSettings(true);
+static const uint8_t CTRL_HUM_ADDR = 0xF2;
+static const uint8_t CTRL_MEAS_ADDR = 0xF4;
+static const uint8_t CONFIG_ADDR = 0xF5;
+static const uint8_t PRESS_ADDR = 0xF7;
+static const uint8_t TEMP_ADDR = 0xFA;
+static const uint8_t HUM_ADDR = 0xFD;
+static const uint8_t TEMP_DIG_ADDR = 0x88;
+static const uint8_t PRESS_DIG_ADDR = 0x8E;
+static const uint8_t HUM_DIG_ADDR1 = 0xA1;
+static const uint8_t HUM_DIG_ADDR2 = 0xE1;
+static const uint8_t ID_ADDR = 0xD0;
 
-  int32_t dummy;
-  uint16_t h;
-  read(dummy, dummy, h);
+static const uint8_t TEMP_DIG_LENGTH = 6;
+static const uint8_t PRESS_DIG_LENGTH = 18;
+static const uint8_t HUM_DIG_ADDR1_LENGTH = 1;
+static const uint8_t HUM_DIG_ADDR2_LENGTH = 7;
+static const uint8_t DIG_LENGTH = 32;
+static const uint8_t SENSOR_DATA_LENGTH = 8;
+
+uint16_t dig_T1;
+int16_t dig_T2;
+int16_t dig_T3;
+
+uint8_t dig_H1;
+int16_t dig_H2;
+uint8_t dig_H3;
+int16_t dig_H4;
+int16_t dig_H5;
+int8_t dig_H6;
+
+uint16_t dig_P1;
+int16_t dig_P2;
+int16_t dig_P3;
+int16_t dig_P4;
+int16_t dig_P5;
+int16_t dig_P6;
+int16_t dig_P7;
+int16_t dig_P8;
+int16_t dig_P9;
+
+/****************************************************************/
+static bool WriteRegister(uint8_t addr, uint8_t data) {
+  TinyI2C.start(BME280_I2C_ADDR, 0);
+  bool ret = TinyI2C.write(addr);
+  if (ret) {
+    ret = TinyI2C.write(data);
+  }
+  TinyI2C.stop();
+  return ret;
 }
-
 /****************************************************************/
-void BME280::WriteSettings(bool filter_off) {
+static bool ReadRegister(uint8_t addr, uint8_t data[], uint8_t length) {
+  // write register address
+  TinyI2C.start(BME280_I2C_ADDR, 0);
+  TinyI2C.write(addr);
+
+  // read data
+  TinyI2C.restart(BME280_I2C_ADDR, length);
+  for (uint8_t i = 0; i < length; ++i) {
+    data[i] = TinyI2C.read();
+  }
+
+  TinyI2C.stop();
+
+  return true;
+}
+/****************************************************************/
+static void WriteSettings() {
   uint8_t ctrlHum, ctrlMeas, config;
 
   // ctrl_hum register. (ctrl_hum[2:0] = Humidity oversampling rate.)
@@ -70,13 +124,7 @@ void BME280::WriteSettings(bool filter_off) {
   ctrlMeas = (TEMP_SAMPLING << 5) | (PRES_SAMPLING << 2) | BME280_MODE;
   // config register. (config[7:5] = standby time, config[4:2] = filter,
   // ctrl_meas[0] = spi enable.)
-  config = (BME280_STANDBY_TIME << 5);
-
-  if (filter_off) {
-    config |= (BME280_FILTER_OFF << 2);
-  } else {
-    config |= (BME280_FILTER << 2);
-  }
+  config = (BME280_STANDBY_TIME << 5) | (BME280_FILTER << 2);
 
   WriteRegister(CTRL_HUM_ADDR, ctrlHum);
   WriteRegister(CTRL_MEAS_ADDR, ctrlMeas);
@@ -84,86 +132,69 @@ void BME280::WriteSettings(bool filter_off) {
 }
 
 /****************************************************************/
-bool BME280::begin() {
-  bool success(true);
-
-  success = ReadTrim();
-
-  InitializeFilter();
-
-  WriteSettings();
-
-  return success;
-}
-
-/****************************************************************/
-bool BME280::ReadTrim() {
+static void ReadTrim() {
   uint8_t ord(0);
-  bool success = true;
+  uint8_t m_dig[32];
 
   // Temp. Dig
-  success &= ReadRegister(TEMP_DIG_ADDR, &m_dig[ord], TEMP_DIG_LENGTH);
+  ReadRegister(TEMP_DIG_ADDR, &m_dig[ord], TEMP_DIG_LENGTH);
   ord += TEMP_DIG_LENGTH;
 
   // Pressure Dig
-  success &= ReadRegister(PRESS_DIG_ADDR, &m_dig[ord], PRESS_DIG_LENGTH);
+  ReadRegister(PRESS_DIG_ADDR, &m_dig[ord], PRESS_DIG_LENGTH);
   ord += PRESS_DIG_LENGTH;
 
   // Humidity Dig 1
-  success &= ReadRegister(HUM_DIG_ADDR1, &m_dig[ord], HUM_DIG_ADDR1_LENGTH);
+  ReadRegister(HUM_DIG_ADDR1, &m_dig[ord], HUM_DIG_ADDR1_LENGTH);
   ord += HUM_DIG_ADDR1_LENGTH;
 
   // Humidity Dig 2
-  success &= ReadRegister(HUM_DIG_ADDR2, &m_dig[ord], HUM_DIG_ADDR2_LENGTH);
+  ReadRegister(HUM_DIG_ADDR2, &m_dig[ord], HUM_DIG_ADDR2_LENGTH);
   ord += HUM_DIG_ADDR2_LENGTH;
 
-  return success && ord == DIG_LENGTH;
+  dig_T1 = (m_dig[1] << 8) | m_dig[0];
+  dig_T2 = (m_dig[3] << 8) | m_dig[2];
+  dig_T3 = (m_dig[5] << 8) | m_dig[4];
+
+  dig_H1 = m_dig[24];
+  dig_H2 = (m_dig[26] << 8) | m_dig[25];
+  dig_H3 = m_dig[27];
+  dig_H4 = (m_dig[28] << 4) | (0x0F & m_dig[29]);
+  dig_H5 = (m_dig[30] << 4) | ((m_dig[29] >> 4) & 0x0F);
+  dig_H6 = m_dig[31];
+
+  dig_P1 = (m_dig[7] << 8) | m_dig[6];
+  dig_P2 = (m_dig[9] << 8) | m_dig[8];
+  dig_P3 = (m_dig[11] << 8) | m_dig[10];
+  dig_P4 = (m_dig[13] << 8) | m_dig[12];
+  dig_P5 = (m_dig[15] << 8) | m_dig[14];
+  dig_P6 = (m_dig[17] << 8) | m_dig[16];
+  dig_P7 = (m_dig[19] << 8) | m_dig[18];
+  dig_P8 = (m_dig[21] << 8) | m_dig[20];
+  dig_P9 = (m_dig[23] << 8) | m_dig[22];
 }
 
-/****************************************************************/
-bool BME280::ReadData(int32_t data[SENSOR_DATA_LENGTH]) {
-  bool success;
-  uint8_t buffer[SENSOR_DATA_LENGTH];
-
-  // Registers are in order. So we can start at the pressure register and read 8
-  // bytes.
-  success = ReadRegister(PRESS_ADDR, buffer, SENSOR_DATA_LENGTH);
-
-  for (int i = 0; i < SENSOR_DATA_LENGTH; ++i) {
-    data[i] = static_cast<int32_t>(buffer[i]);
-  }
-
-  return success;
-}
-
-/****************************************************************/
-int32_t BME280::CalculateTemperature(int32_t raw, int32_t &t_fine) {
+// Returns temperature in °C, resolution is 0.01 DegC. Output value of “5123” equals 51.23 °C.
+static int32_t CalculateTemperature(int32_t raw, int32_t &t_fine) {
   // Code based on calibration algorthim provided by Bosch.
   int32_t var1, var2, final;
-  uint16_t dig_T1 = (m_dig[1] << 8) | m_dig[0];
-  int16_t dig_T2 = (m_dig[3] << 8) | m_dig[2];
-  int16_t dig_T3 = (m_dig[5] << 8) | m_dig[4];
   var1 = ((((raw >> 3) - ((int32_t)dig_T1 << 1))) * ((int32_t)dig_T2)) >> 11;
   var2 =
       (((((raw >> 4) - ((int32_t)dig_T1)) * ((raw >> 4) - ((int32_t)dig_T1))) >>
         12) *
        ((int32_t)dig_T3)) >>
       14;
+
   t_fine = var1 + var2;
   final = (t_fine * 5 + 128) >> 8;
   return final;
 }
 
-/****************************************************************/
-uint16_t BME280::CalculateHumidity(uint32_t raw, int32_t t_fine) {
+// Returns humidity in %RH as unsigned 32 bit integer in Q22.10 format (22 integer and 10 fractional bits).
+// Output value of “47445” represents 47445/1024 = 46.333 %RH
+static uint16_t CalculateHumidity(uint32_t raw, int32_t t_fine) {
   // Code based on calibration algorthim provided by Bosch.
   int32_t var1;
-  uint8_t dig_H1 = m_dig[24];
-  int16_t dig_H2 = (m_dig[26] << 8) | m_dig[25];
-  uint8_t dig_H3 = m_dig[27];
-  int16_t dig_H4 = (m_dig[28] << 4) | (0x0F & m_dig[29]);
-  int16_t dig_H5 = (m_dig[30] << 4) | ((m_dig[29] >> 4) & 0x0F);
-  int8_t dig_H6 = m_dig[31];
 
   var1 = (t_fine - ((int32_t)76800));
   var1 = (((((raw << 14) - (((int32_t)dig_H4) << 20) -
@@ -185,114 +216,87 @@ uint16_t BME280::CalculateHumidity(uint32_t raw, int32_t t_fine) {
   return (uint16_t)(((uint32_t)(var1 >> 12)) / 1024.0 * 100);
 }
 
-/****************************************************************/
-int32_t BME280::CalculatePressure(int32_t raw, int32_t t_fine) {
-  // Code based on calibration algorthim provided by Bosch.
-  int64_t var1, var2, pressure;
-  int32_t final;
+// Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24 integer bits and 8 fractional bits).
+// Output value of “24674867” represents 24674867/256 = 96386.2 Pa = 963.862 hPa
+static int32_t CalculatePressure(int32_t raw, int32_t t_fine) {
+  // Code based on 8bit calibration algorthim provided by Bosch.
+  int32_t var1, var2;
+  uint32_t p;
+  var1 = (((int32_t)t_fine) >> 1) - (int32_t)64000;
+  var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * ((int32_t)dig_P6);
+  var2 = var2 + ((var1 * ((int32_t)dig_P5)) << 1);
+  var2 = (var2 >> 2) + (((int32_t)dig_P4) << 16);
+  var1 = (((dig_P3 * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) +
+          ((((int32_t)dig_P2) * var1) >> 1)) >>
+         18;
+  var1 = ((((32768 + var1)) * ((int32_t)((uint16_t)dig_P1))) >> 15);
+  if (var1 == 0)
+    return 0;
+  p = (((uint32_t)(((int32_t)1048576) - raw) - (var2 >> 12))) * 3125;
+  if (p < 0x80000000)
+    p = (p << 1) / ((uint32_t)var1);
+  else
+    p = (p / (uint32_t)var1) * 2;
+  var1 = (((int32_t)dig_P9) * ((int32_t)(((p >> 3) * (p >> 3)) >> 13))) >> 12;
+  var2 = (((int32_t)(p >> 2)) * ((int32_t)dig_P8)) >> 13;
+  p = (uint32_t)((int32_t)p + ((var1 + var2 + dig_P7) >> 4));
 
-  uint16_t dig_P1 = (m_dig[7] << 8) | m_dig[6];
-  int16_t dig_P2 = (m_dig[9] << 8) | m_dig[8];
-  int16_t dig_P3 = (m_dig[11] << 8) | m_dig[10];
-  int16_t dig_P4 = (m_dig[13] << 8) | m_dig[12];
-  int16_t dig_P5 = (m_dig[15] << 8) | m_dig[14];
-  int16_t dig_P6 = (m_dig[17] << 8) | m_dig[16];
-  int16_t dig_P7 = (m_dig[19] << 8) | m_dig[18];
-  int16_t dig_P8 = (m_dig[21] << 8) | m_dig[20];
-  int16_t dig_P9 = (m_dig[23] << 8) | m_dig[22];
-
-  var1 = (int64_t)t_fine - 128000;
-  var2 = var1 * var1 * (int64_t)dig_P6;
-  var2 = var2 + ((var1 * (int64_t)dig_P5) << 17);
-  var2 = var2 + (((int64_t)dig_P4) << 35);
-  var1 =
-      ((var1 * var1 * (int64_t)dig_P3) >> 8) + ((var1 * (int64_t)dig_P2) << 12);
-  var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)dig_P1) >> 33;
-  if (var1 == 0) {
-    return -1;
-  } // Don't divide by zero.
-  pressure = 1048576 - raw;
-  pressure = (((pressure << 31) - var2) * 3125) / var1;
-  var1 = (((int64_t)dig_P9) * (pressure >> 13) * (pressure >> 13)) >> 25;
-  var2 = (((int64_t)dig_P8) * pressure) >> 19;
-  pressure = ((pressure + var1 + var2) >> 8) + (((int64_t)dig_P7) << 4);
-
-  final = ((uint32_t)pressure) / 256.0;
-
-  // return hPa * 100
-  return final;
+  return p;
 }
 
 /****************************************************************/
-int32_t BME280::pres(int32_t &temp) {
-  int32_t data[8];
+bool BME280_begin() {
+  ReadTrim();
+  WriteSettings();
+
+  return true;
+}
+
+int32_t BME280_pres(int32_t &temp) {
+  uint8_t data[SENSOR_DATA_LENGTH];
+  ReadRegister(PRESS_ADDR, data, SENSOR_DATA_LENGTH);
+
+  int32_t rawTemp =
+      ((int32_t)(data[3]) << 12) | (data[4] << 4) | (data[5] >> 4);
+  uint32_t rawPressure =
+      ((int32_t)(data[3]) << 12) | (data[4] << 4) | (data[5] >> 4);
+
   int32_t t_fine;
-  if (!ReadData(data)) {
-    return -1;
-  }
-  uint32_t rawTemp = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
-  uint32_t rawPressure = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4);
   temp = CalculateTemperature(rawTemp, t_fine);
   return CalculatePressure(rawPressure, t_fine);
 }
 
-/****************************************************************/
-uint16_t BME280::hum(int32_t &temp) {
-  int32_t data[8];
-  int32_t t_fine;
-  if (!ReadData(data)) {
-    return -1;
-  }
-  uint32_t rawTemp = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
+uint16_t BME280_hum(int32_t &temp) {
+  uint8_t data[SENSOR_DATA_LENGTH];
+  ReadRegister(PRESS_ADDR, data, SENSOR_DATA_LENGTH);
+
+  int32_t rawTemp =
+      ((int32_t)(data[3]) << 12) | (data[4] << 4) | (data[5] >> 4);
   uint32_t rawHumidity = (data[6] << 8) | data[7];
+
+  int32_t t_fine;
   temp = CalculateTemperature(rawTemp, t_fine);
   return CalculateHumidity(rawHumidity, t_fine);
 }
 
 /****************************************************************/
-void BME280::read(int32_t &pressure, int32_t &temp, uint16_t &humidity) {
-  int32_t data[8];
-  int32_t t_fine;
-  if (!ReadData(data)) {
-    return;
-  }
-  uint32_t rawPressure = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4);
-  uint32_t rawTemp = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
+void BME280_read(int32_t &pressure, int32_t &temp, uint16_t &humidity) {
+  uint8_t data[SENSOR_DATA_LENGTH];
+  ReadRegister(PRESS_ADDR, data, SENSOR_DATA_LENGTH);
+
+  int32_t rawPressure =
+      ((int32_t)(data[0]) << 12) | (data[1] << 4) | (data[2] >> 4);
+  int32_t rawTemp =
+      ((int32_t)(data[3]) << 12) | (data[4] << 4) | (data[5] >> 4);
   uint32_t rawHumidity = (data[6] << 8) | data[7];
+
+  int32_t t_fine;
   temp = CalculateTemperature(rawTemp, t_fine);
   pressure = CalculatePressure(rawPressure, t_fine);
   humidity = CalculateHumidity(rawHumidity, t_fine);
 }
 
-/****************************************************************/
-bool BME280::WriteRegister(uint8_t addr, uint8_t data) {
-  TinyI2C.start(BME280_I2C_ADDR, 0);
-  bool ret = TinyI2C.write(addr);
-  if (ret) {
-    ret = TinyI2C.write(data);
-  }
-  TinyI2C.stop();
-  return ret;
-}
-
-/****************************************************************/
-bool BME280::ReadRegister(uint8_t addr, uint8_t data[], uint8_t length) {
-  // write register address
-  TinyI2C.start(BME280_I2C_ADDR, 0);
-  TinyI2C.write(addr);
-
-  // read data
-  TinyI2C.restart(BME280_I2C_ADDR, length);
-  for (uint8_t i = 0; i < length; ++i) {
-    data[i] = TinyI2C.read();
-  }
-
-  TinyI2C.stop();
-
-  return true;
-}
-
-BME280::ChipModel BME280::chipModel() {
+uint8_t BME280_chipModel() {
 
   uint8_t id[1];
 
