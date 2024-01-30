9010b7fd7088715a03a0b61542360d595cfc7fef
JC Menchi <jean.menchi@gmail.com>
2024-01-30 21:49:45 +0100

    Send sonar value

--- ATmega8515/Bot1/src/main.cpp
+++ ATmega8515/Bot1/src/main.cpp
@@ -129,26 +129,24 @@ void setup()
     pinMode(TxPin, OUTPUT);
     vw_set_tx_pin(TxPin);
     // Initialise the IO and ISR
-    //vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);	 // Bits per sec
+    vw_setup(500);	 // Bits per sec
 }
+
+uint8_t count = 0;
 
 void loop()
 {
-    const char *msg = "hello1234";
-    const char *msg2 = "world1234";
+    char msg[64];
+    long cm = sonar.ping_cm();
 
+    snprintf(msg, 64, "%d: %d", count, cm);
     digitalWrite(LED_PIN, true); // Flash a light to show transmitting
-    Serial.println("send hello");
+    Serial.print("send: ");
+    Serial.println(msg);
     vw_send((uint8_t *)msg, strlen(msg));
     vw_wait_tx(); // Wait until the whole message is gone
     delay(2000);
-    digitalWrite(LED_PIN, false);
-    Serial.println("send world");
-    vw_send((uint8_t *)msg2, strlen(msg2));
-    vw_wait_tx(); // Wait until the whole message is gone
-    digitalWrite(LED_PIN, false);
-    delay(2000);
+    count++;
 }
 
 /*
