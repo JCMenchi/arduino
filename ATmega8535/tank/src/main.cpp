e342bea156ec33bf561e1fb7aec43593d9005300
JC Menchi <jean.menchi@gmail.com>
2025-02-09 18:24:40 +0100

    Add Tank class

--- ATmega8535/tank/src/main.cpp
+++ ATmega8535/tank/src/main.cpp
@@ -1,80 +1,160 @@
 
 
-#include <util/delay.h>
-
+#include <SSD1306Display.h>
 #include <avr/io.h>
+#include <gpio.h>
+#include <millisec.h>
+#include <pwm.h>
+#include <shiftregister.h>
 #include <stdlib.h>
 #include <string.h>
+#include <usart_serial.h>
+#include <util/delay.h>
 
-#include <gpio.h>
-#include "usart_serial.h"
-#include <shiftregister.h>
-
+#include "tank.h"
 
 #ifndef LED_PORTID
 #define LED_PORTID A
 #endif
-
-const uint8_t ON_LED_PIN = 0;
-
-const uint8_t DATA_PIN = 1;  // DS
-const uint8_t LATCH_PIN = 2; // ST_CP
-const uint8_t CLOCK_PIN = 3; // SH_CP
-
-ShiftRegisterPort shiftreg(DATA_PIN, LATCH_PIN, CLOCK_PIN);
-
-#define SR_Q0 0x01
-#define SR_Q1 0x02
-#define SR_Q2 0x04
-#define SR_Q3 0x08
-#define SR_Q4 0x10
-#define SR_Q5 0x20
-#define SR_Q6 0x40
-#define SR_Q7 0x80
+#include <avr/pgmspace.h>
+#define ON_LED_PIN 0
 
 #define ON_LED SR_Q0
 
-#define MOTOR1_FORWARD SR_Q1
-#define MOTOR1_REVERSE SR_Q2
-#define MOTOR1_ON SR_Q5
+Tank tank;
 
-#define MOTOR2_FORWARD SR_Q3
-#define MOTOR2_REVERSE SR_Q4
-#define MOTOR2_ON SR_Q6
+SSD1306Display display(128, 32);
 
 void setup() {
-  // init debug LED
-  GPIO_OUTPUT(LED_PORTID, ON_LED_PIN);
+    // init debug LED
+    GPIO_OUTPUT(LED_PORTID, ON_LED_PIN);
 
-  // startup blinking of ON LED
-  GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
-  _delay_ms(200);
-  GPIO_SET_LOW(LED_PORTID, ON_LED_PIN);
-  _delay_ms(500);
-  GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
-  _delay_ms(200);
-  GPIO_SET_LOW(LED_PORTID, ON_LED_PIN);
-  _delay_ms(500);
-  GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
-  
-  // init serial com
-  USART_Init(BAUD_RATE_57600, SerialCommandMgr::serialInput);
-  USART_WriteString("ATmega8535 serial com ready\n");
+    // startup blinking of ON LED
+    GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
+    _delay_ms(200);
+    GPIO_SET_LOW(LED_PORTID, ON_LED_PIN);
+    _delay_ms(500);
+    GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
+    _delay_ms(200);
+    GPIO_SET_LOW(LED_PORTID, ON_LED_PIN);
+    _delay_ms(500);
+    GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
 
-  shiftreg.setup();
-  shiftreg.allLow();
+    // init serial com; only 9600 for ATmega8535 (is it a bug?) 
+    USART_Init(BAUD_RATE_9600, SerialCommandMgr::serialInput);
+    USART_WritePString(PSTR("ATmega8535 serial com ready\n"));
 
-  //USART_WriteString("ATmega8535 ready\n");
+    display.init();
+    display.invert(SSD1306_OFF);
+    display.flip(SSD1306_OFF);
+
+    display.drawScreen(0x00, false);
+    display.drawPString(60, 0, PSTR("ATmega8535"));
+    tank.setup();
+}
+
+#define STATUS_CMD "status"
+#define LEFT_CMD "l"
+#define RIGHT_CMD "r"
+#define FORWARD_CMD "f"
+#define BACKWARD_CMD "b"
+#define STOP_CMD "S"
+#define ACCEL_CMD "+"
+#define DECEL_CMD "-"
+
+void execCommand(uint32_t now, const char *cmd) {
+    // check if command is defined
+    if (cmd == NULL || strlen(cmd) == 0)
+        return;
+
+    // USART_WriteString(cmd);
+    display.drawPage(2, 0x00);
+    display.drawString(0, 16, ">");
+    display.drawString(10, 16, cmd);
+
+    if (strcmp(cmd, STATUS_CMD) == 0) {
+        tank.info();
+    } else if (strcmp(cmd, FORWARD_CMD) == 0) {
+        tank.forward();
+    } else if (strcmp(cmd, BACKWARD_CMD) == 0) {
+        tank.backward();
+    } else if (strcmp(cmd, LEFT_CMD) == 0) {
+        tank.left();
+    } else if (strcmp(cmd, RIGHT_CMD) == 0) {
+        tank.right();
+    } else if (strcmp(cmd, STOP_CMD) == 0) {
+        tank.stop();
+    } else if (strcmp(cmd, ACCEL_CMD) == 0) {
+        tank.accel();
+    } else if (strcmp(cmd, DECEL_CMD) == 0) {
+        tank.decel();
+    }
+    tank.info();
+    tank.display(&display);
+}
+
+uint8_t on = 0;
+
+uint32_t prev = 0;
+
+void showState(uint32_t now, uint8_t isOn) {
+    uint8_t sec = (now / 1000) % 60;
+    uint8_t min = ((now / 1000) / 60) % 60;
+    uint8_t hour = (((now / 1000) / 60) / 60);
+
+    if (isOn == 0) {
+        GPIO_SET_LOW(LED_PORTID, ON_LED_PIN);
+        // USART_WritePString(PSTR("OFF\n"));
+        //display.drawPage(1, 0x00);
+    } else {
+        GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
+        // USART_WritePString(PSTR("ON\n"));
+        //display.drawPage(1, 0x00);
+    }
+
+    const uint8_t base = 0;
+    const uint8_t line = 0;
+
+    if (hour >= 10) {
+      display.drawInt(base, line, hour, 10);
+    } else {
+      display.drawString(base, line, "0");
+      display.drawInt(base + 5, line, hour, 10);
+    }
+    display.drawString(base + 2*5, line, ":");
+    
+    if (min >= 10) {
+      display.drawInt(base + 3*5, line, min, 10);
+    } else {
+      display.drawString(base + 3*5, line, "0");
+      display.drawInt(base + 4*5, line, min, 10);
+    }
+
+    display.drawString(base + 5*5, line, ":");
+
+    if (sec >= 10) {
+      display.drawInt(base + 6*5, line, sec, 10);
+    } else {
+      display.drawString(base + 6*5, line, "0");
+      display.drawInt(base + 7*5, line, sec, 10);
+    }
 }
 
 void loop() {
-  GPIO_SET_LOW(LED_PORTID, ON_LED_PIN);
-  shiftreg.setMask(ON_LED);
-  _delay_ms(500);
+    uint32_t now = milliseconds();
 
-  GPIO_SET_HIGH(LED_PORTID, ON_LED_PIN);
-  shiftreg.setMask(0);
-  _delay_ms(500);
+    if ((now - prev) > 1000) {
+        on = (on == 0) ? 1 : 0;
+        showState(now, on);
+        prev = now;
+    }
+
+    // exec user commands
+    if (SerialCommandMgr::hasCommand()) {
+        execCommand(now, SerialCommandMgr::command());
+    }
+
+    _delay_ms(10);
 }
 
 #include <main.cpp.h>
---
+++ ATmega8535/tank/src/tank.h
@@ -0,0 +1,200 @@
+#ifndef TANK_H
+#define TANK_H
+
+#include <gpio.h>
+#include <pwm.h>
+#include <shiftregister.h>
+#include <usart_serial.h>
+#include <SSD1306Display.h>
+
+#ifndef MOTOR1_FORWARD
+#define MOTOR1_FORWARD SR_Q1
+#endif
+
+#ifndef MOTOR1_REVERSE
+#define MOTOR1_REVERSE SR_Q2
+#endif
+
+#ifndef MOTOR1_ON
+#define MOTOR1_ON SR_Q5
+#endif
+
+#ifndef MOTOR2_FORWARD
+#define MOTOR2_FORWARD SR_Q3
+#endif
+
+#ifndef MOTOR2_REVERSE
+#define MOTOR2_REVERSE SR_Q4
+#endif
+
+#ifndef MOTOR2_ON
+#define MOTOR2_ON SR_Q6
+#endif
+
+#ifndef MOTOR_SPEED_CONTROL
+#define MOTOR_SPEED_CONTROL PWM_OC2
+#endif
+
+#ifndef DATA_PIN
+#define DATA_PIN 1  // DS
+#endif
+
+#ifndef LATCH_PIN
+#define LATCH_PIN 2  // ST_CP
+#endif
+
+#ifndef CLOCK_PIN
+#define CLOCK_PIN 3  // SH_CP
+#endif
+
+
+const uint8_t minSpeed = 150;
+const uint8_t maxSpeed = 255;
+
+class Tank {
+public:
+    Tank() : m_speed(minSpeed), shiftreg(DATA_PIN, LATCH_PIN, CLOCK_PIN) {}
+
+
+    inline void info() {
+        USART_WriteString("Tank is: ");
+        if (shiftreg.getState(MOTOR1_ON) == 1 && shiftreg.getState(MOTOR2_ON) == 1) {
+            if (shiftreg.getState(MOTOR1_FORWARD) == 1 && shiftreg.getState(MOTOR2_FORWARD) == 1) {
+                USART_WriteString("going forward");
+            } else if (shiftreg.getState(MOTOR1_REVERSE) == 1 && shiftreg.getState(MOTOR2_REVERSE) == 1) {
+                USART_WriteString("going backward");
+            }
+        } else if (shiftreg.getState(MOTOR1_ON) == 1 && shiftreg.getState(MOTOR2_ON) == 0) {
+            USART_WriteString(" turning left");
+        } else if (shiftreg.getState(MOTOR1_ON) == 0 && shiftreg.getState(MOTOR2_ON) == 1) {
+            USART_WriteString(" turning right");
+        } else {
+            USART_WriteString("stopped");
+        }
+
+        USART_WriteString(", speed: ");
+        USART_WriteUInt(m_speed, 10);
+        USART_WriteString("\n");
+    }
+
+    void display(SSD1306Display *display) {
+        display->drawPage(3, 0x00);
+        display->drawString(0,24, "Tank:");
+        if (shiftreg.getState(MOTOR1_ON) == 1 && shiftreg.getState(MOTOR2_ON) == 1) {
+            if (shiftreg.getState(MOTOR1_FORWARD) == 1 && shiftreg.getState(MOTOR2_FORWARD) == 1) {
+                display->drawString(7*5,24,"forward");
+            } else if (shiftreg.getState(MOTOR1_REVERSE) == 1 && shiftreg.getState(MOTOR2_REVERSE) == 1) {
+                display->drawString(7*5,24,"backward");
+            }
+        } else if (shiftreg.getState(MOTOR1_ON) == 1 && shiftreg.getState(MOTOR2_ON) == 0) {
+            display->drawString(7*5,24,"left");
+        } else if (shiftreg.getState(MOTOR1_ON) == 0 && shiftreg.getState(MOTOR2_ON) == 1) {
+            display->drawString(7*5,24,"right");
+        } else {
+            display->drawString(7*5,24,"stopped");
+        }
+
+        display->drawString(17*5,24,"sp:");
+        display->drawInt(21*5,24,m_speed, 10);
+    }
+
+    inline void setup() {
+        shiftreg.setup();
+        shiftreg.allLow();
+        shiftreg.setMask(0x01);
+        enablePWM(MOTOR_SPEED_CONTROL);  // PORT D 7
+    }
+
+    ShiftRegisterPort* shiftregistry() {
+        return &shiftreg;
+    }
+
+    inline void forward() {
+        setPWM(MOTOR_SPEED_CONTROL, 0);
+
+        shiftreg.setState(MOTOR1_FORWARD, 1);
+        shiftreg.setState(MOTOR1_REVERSE, 0);
+        shiftreg.setState(MOTOR1_ON, 1);
+        shiftreg.setState(MOTOR2_FORWARD, 1);
+        shiftreg.setState(MOTOR2_REVERSE, 0);
+        shiftreg.setState(MOTOR2_ON, 1);
+
+        shiftreg.sendData();
+
+        setPWM(MOTOR_SPEED_CONTROL, m_speed);
+    }
+
+    inline void backward() {
+        setPWM(MOTOR_SPEED_CONTROL, 0);
+        shiftreg.setState(MOTOR1_FORWARD, 0);
+        shiftreg.setState(MOTOR1_REVERSE, 1);
+        shiftreg.setState(MOTOR1_ON, 1);
+        shiftreg.setState(MOTOR2_FORWARD, 0);
+        shiftreg.setState(MOTOR2_REVERSE, 1);
+        shiftreg.setState(MOTOR2_ON, 1);
+        shiftreg.sendData();
+
+        setPWM(MOTOR_SPEED_CONTROL, m_speed);
+    }
+
+    inline void left() {
+        
+        shiftreg.setState(MOTOR1_FORWARD, 1);
+        shiftreg.setState(MOTOR1_REVERSE, 0);
+        shiftreg.setState(MOTOR1_ON, 1);
+        shiftreg.setState(MOTOR2_FORWARD, 0);
+        shiftreg.setState(MOTOR2_REVERSE, 0);
+        shiftreg.setState(MOTOR2_ON, 0);
+        shiftreg.sendData();
+
+        setPWM(MOTOR_SPEED_CONTROL, m_speed);
+    }
+
+    inline void right() {
+        setPWM(MOTOR_SPEED_CONTROL, 0);
+        shiftreg.setState(MOTOR1_FORWARD, 0);
+        shiftreg.setState(MOTOR1_REVERSE, 0);
+        shiftreg.setState(MOTOR1_ON, 0);
+        shiftreg.setState(MOTOR2_FORWARD, 1);
+        shiftreg.setState(MOTOR2_REVERSE, 0);
+        shiftreg.setState(MOTOR2_ON, 1);
+        shiftreg.sendData();
+
+        setPWM(MOTOR_SPEED_CONTROL, m_speed);
+    }
+
+    inline void stop() {
+        setPWM(MOTOR_SPEED_CONTROL, 0);
+        shiftreg.setState(MOTOR1_FORWARD, 0);
+        shiftreg.setState(MOTOR1_REVERSE, 0);
+        shiftreg.setState(MOTOR1_ON, 0);
+        shiftreg.setState(MOTOR2_FORWARD, 0);
+        shiftreg.setState(MOTOR2_REVERSE, 0);
+        shiftreg.setState(MOTOR2_ON, 0);
+        shiftreg.sendData();
+    }
+
+    inline void accel() {
+        if (m_speed > maxSpeed - 10) {
+            m_speed = maxSpeed;
+        } else {
+            m_speed += 10;
+        }
+        setPWM(MOTOR_SPEED_CONTROL, m_speed);
+    }
+
+    inline void decel() {
+        if (m_speed < minSpeed) {
+            m_speed = minSpeed;
+        } else {
+            m_speed -= 10;
+        }
+        setPWM(MOTOR_SPEED_CONTROL, m_speed);
+    }
+
+   private:
+    uint8_t m_speed;
+    ShiftRegisterPort shiftreg;
+};
+
+#endif
